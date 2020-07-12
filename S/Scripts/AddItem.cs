e82ff9ff37cf74681f7d3c5379ec416cb045d415
Nyrmehametov danil <danilka468@outlook.com>
2020-07-12 03:20:59 +0300

    THE END

--- S/Scripts/AddItem.cs
+++ S/Scripts/AddItem.cs
@@ -11,48 +11,64 @@ namespace S.Scripts
 {
     class AddItem
     {
-        static public string path, FileName, word;
+        static public string path, FileName, word, language;
         static public List<string> arbah = new List<string>();
-      
+      static public List<string> Translate = new List<string>();
         static  public void add()
         {
             Console.Clear();
             Console.WriteLine("Добавление слов");
-            using (SqlConnection connection = new SqlConnection("Data Source=DVAR; Initial Catalog=Proba; Integrated Security=True; "))
+
+
+            Console.Write("Введите язык слов который будет добавляться(RU-EN):");
+            language=Console.ReadLine();
+
+            if (language == "RU")
             {
-                connection.Open();
-                connection.Close();
+                adRU();
             }
+            else
+            {
+                adEN();
+            }
+
+        }
+
+
+
+
+        static public void adRU()
+        {
             Console.Write("Ведите путь к папке с файлом новых слов:");
-            path= Console.ReadLine();
+            path = Console.ReadLine();
 
             Console.Write("Полное имя файла:");
             FileName = Console.ReadLine();
-            path+=@"\";
-           
-                using (StreamReader SR = new StreamReader(path + FileName))
-                {
-                    word = SR.ReadToEnd();
+            path += @"\";
 
-
-                }
-                arbah.AddRange(word.Split("\n"));
-
-            try 
+            using (StreamReader SR = new StreamReader(path + FileName))
             {
-               
-               for (int i = 0; i < arbah.Count; i++)
-               {
-                    string sql = "INSERT try VALUES('" +arbah[i]+ "')";
+                word = SR.ReadToEnd();
 
-                        using (SqlConnection connection = new SqlConnection("Data Source=DVAR; Initial Catalog=Proba; Integrated Security=True"))
-                        {
-                            connection.Open();
-                                SqlCommand command = new SqlCommand(sql, connection);
-                                command.ExecuteNonQuery();
-                            connection.Close();
-                        }
-               }
+
+            }
+            arbah.AddRange(word.Split("\n"));
+
+            try
+            {
+
+                for (int i = 0; i < arbah.Count; i++)
+                {
+                    string sql = "INSERT try VALUES('" + arbah[i] + "')";
+
+                    using (SqlConnection connection = new SqlConnection("Data Source=DVAR; Initial Catalog=Proba; Integrated Security=True"))
+                    {
+                        connection.Open();
+                        SqlCommand command = new SqlCommand(sql, connection);
+                        command.ExecuteNonQuery();
+                        connection.Close();
+                    }
+                }
                 Console.WriteLine("\r\n" + "Все загружено");
                 Thread.Sleep(1000);
                 Console.WriteLine("Через 2 секунды вернется главное меню");
@@ -65,12 +81,64 @@ namespace S.Scripts
                 MainMenu.maiMenu();
             }
             catch
-            { 
+            {
                 Console.WriteLine("что-то пошло не так");
             }
-
         }
 
+        static public void adEN()
+        {
+            Console.WriteLine("Пример ввода слова: EN-RU");
+            Console.Write("Ведите путь к папке с файлом новых слов:");
+            path = Console.ReadLine();
+
+            Console.Write("Полное имя файла:");
+            FileName = Console.ReadLine();
+            path += @"\";
+
+            using (StreamReader SR = new StreamReader(path + FileName))
+            {
+                word = SR.ReadToEnd();
+               
+
+            }
+         
+            arbah.AddRange(word.Split("\n"));
+         
+
+            try
+            {
+
+                for (int i = 0; i < arbah.Count; i++)
+                {
+                    string word = arbah[i].Split(" ")[0];
+                    string translating = arbah[i].Substring(arbah[i].IndexOf(" ") + 1);
+                    string sql = "INSERT tryEN(Words, Translate) VALUES('" + word + "', '"+translating+"')";
+
+                    using (SqlConnection connection = new SqlConnection("Data Source=DVAR; Initial Catalog=Proba; Integrated Security=True"))
+                    {
+                        connection.Open();
+                        SqlCommand command = new SqlCommand(sql, connection);
+                        command.ExecuteNonQuery();
+                        connection.Close();
+                    }
+                }
+                Console.WriteLine("\r\n" + "Все загружено");
+                Thread.Sleep(1000);
+                Console.WriteLine("Через 2 секунды вернется главное меню");
+                Thread.Sleep(1000);
+                Console.WriteLine("1 СЕКУНДА");
+                Thread.Sleep(1000);
+                Console.WriteLine("ВОЗВРАТ");
+                Thread.Sleep(1000);
+
+                MainMenu.maiMenu();
+            }
+            catch
+            {
+                Console.WriteLine("что-то пошло не так");
+            }
+        }
 
     }
 }
--- S/Scripts/ClearBD.cs
+++ S/Scripts/ClearBD.cs
@@ -9,26 +9,46 @@ namespace S.Scripts
 {
     class ClearBD
     {
-
+        static public string bd;
         static public void delet()
         {
             Console.Clear();
+            Console.WriteLine("Какую базу очистить RU-EN");
+            string language = Console.ReadLine();
+            if (language == "RU" || language == "ru" || language == "Ru")
+            {
+                Console.WriteLine("Очищение русской базы");
+                bd = "[Proba].[dbo].[try]";
+                alice();
+            }
+
+            else 
+            {
+                Console.WriteLine("Очищение английской базы");
+                bd = "[Proba].[dbo].[tryEN]";
+                alice();
+            }
+        }
+
+
+        static public void alice()
+        {
             try
             {
                 int jk;
-                string sql = "DELETE [Proba].[dbo].[try]";
+                string sql = "DELETE "+bd;
 
-                using (SqlConnection connection=new SqlConnection("Data Source = DVAR; Initial Catalog = Proba; Integrated Security = True;"))
+                using (SqlConnection connection = new SqlConnection("Data Source = DVAR; Initial Catalog = Proba; Integrated Security = True;"))
                 {
                     connection.Open();
-                        SqlCommand command = new SqlCommand(sql, connection);
-                        jk=  command.ExecuteNonQuery();
+                    SqlCommand command = new SqlCommand(sql, connection);
+                    jk = command.ExecuteNonQuery();
                     connection.Close();
                 }
 
                 Console.WriteLine("База очищена");
 
-                Console.WriteLine("\r\n" +"Удаленно "+ jk+" слов");
+                Console.WriteLine("\r\n" + "Удаленно " + jk + " слов");
                 Thread.Sleep(1000);
                 Console.WriteLine("Через 2 секунды вернется главное меню");
                 Thread.Sleep(1000);
